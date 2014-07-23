4908915e86c9818bee651bdd517cf4779727b965
Jeffrey Kegler <JKEGL@cpan.org>
2014-07-22 17:54:40 -0700

    Update licenses

--- etc/libmarpa/License.pm
+++ etc/libmarpa/License.pm
@@ -1,16 +1,16 @@
 # Copyright 2014 Jeffrey Kegler
-# This file is part of Libmarpa.  Libmarpa is free software: you can
+# This file is part of Kollos.  Kollos is free software: you can
 # redistribute it and/or modify it under the terms of the GNU Lesser
 # General Public License as published by the Free Software Foundation,
 # either version 3 of the License, or (at your option) any later version.
 #
-# Libmarpa is distributed in the hope that it will be useful,
+# Kollos is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser
-# General Public License along with Libmarpa.  If not, see
+# General Public License along with Kollos.  If not, see
 # http://www.gnu.org/licenses/.
 
 package libmarpa::Internal::License;
@@ -29,7 +29,7 @@ my $copyright_line = q{Copyright 2014 Jeffrey Kegler};
     =~ s/ ^ Copyright \s /Copyright \\copyright\\ /xms;
 
 my $closed_license = "$copyright_line\n" . <<'END_OF_STRING';
-This document is not part of the Marpa or Libmarpa source.
+This document is not part of the Marpa or Kollos source.
 Although it may be included with a Marpa distribution that
 is under an open source license, this document is
 not under that open source license.
@@ -37,18 +37,18 @@ Jeffrey Kegler retains full rights.
 END_OF_STRING
 
 my $license_body = <<'END_OF_STRING';
-This file is part of Libmarpa.  Libmarpa is free software: you can
+This file is part of Kollos.  Kollos is free software: you can
 redistribute it and/or modify it under the terms of the GNU Lesser
 General Public License as published by the Free Software Foundation,
 either version 3 of the License, or (at your option) any later version.
 
-Libmarpa is distributed in the hope that it will be useful,
+Kollos is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser
-General Public License along with Libmarpa.  If not, see
+General Public License along with Kollos.  If not, see
 http://www.gnu.org/licenses/.
 END_OF_STRING
 
@@ -61,7 +61,7 @@ $license_in_tex =~ s/^$/\\smallskip\\noindent/gxms;
 
 my $license_file = $license . <<'END_OF_STRING';
 
-In the Libmarpa distribution, the GNU Lesser General Public License
+In the Kollos distribution, the GNU Lesser General Public License
 version 3 should be in a file named "COPYING.LESSER".
 END_OF_STRING
 
